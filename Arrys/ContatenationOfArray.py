58001994b49e0f4e1ef6bfe247ca0470f72b81af
sanjit <sanjit.est14@gmail.com>
2026-03-09 13:28:00 +0600

    More Concise way

--- Arrys/ContatenationOfArray.py
+++ Arrys/ContatenationOfArray.py
@@ -1,7 +1,12 @@
+# class Solution:
+#     def getConcatenation(self, nums: List[int]) -> List[int]:
+#         ans = [0] * (2 * len(nums))
+#         for i, n in enumerate(nums):
+#             ans[i] = n
+#             ans[i + len(nums)] = n
+#         return ans
+
+
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = [0] * (2 * len(nums))
-        for i, n in enumerate(nums):
-            ans[i] = n
-            ans[i + len(nums)] = n
-        return ans
+        return nums+nums
